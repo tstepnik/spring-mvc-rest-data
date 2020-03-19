f2fdecd1cbc54160c256b9a0b4d4db97ed52c73f
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-19 15:05:08 +0100

    essential files

---
+++ src/main/java/pl/javastart/SpringMvcRestDataApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcRestDataApplication.class, args);
+    }
+
+}
